a073397a9f28c17b08626e9a9b311a2951673d1f
Ahmed Ibreljic <ahmedibreljic@gmail.com>
2023-10-23 17:41:55 -0400

    Update ai.py

--- src/ai.py
+++ src/ai.py
@@ -2,7 +2,7 @@ from ai_bricks.api import openai
 import stats
 import os
 
-DEFAULT_USER = os.getenv('COMMUNITY_USER','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')
+DEFAULT_USER = os.getenv('COMMUNITY_USER','')
 
 def use_key(key):
 	openai.use_key(key)
